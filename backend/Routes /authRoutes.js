5c0cbe5cd3e0256a797b628f5c66739489d0c131
ameek <ameekajaz2@gmail.com>
2026-09-24 22:35:29 +0530

    fix the route

--- backend/Routes /authRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { register, login } = require('../controllers/authController');
-
-const Router = express.Router();
-
-Router.post('/register', register);
-Router.post('/login', login);
-
-module.exports = Router;
